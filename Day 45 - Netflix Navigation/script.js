66856e3d82bde8bd25c3f63cb7f6b6ead038cd2c
Ravi Teja Karanam <50255591+RaviTejaKaranam@users.noreply.github.com>
2021-02-26 01:44:43 +0530

    Add files via upload

---
+++ Day 45 - Netflix Navigation/script.js
@@ -0,0 +1,16 @@
+const openBtn = document.querySelector(".open-btn")
+const closeBtn = document.querySelector(".close-btn")
+const navBars = document.querySelectorAll(".nav")
+
+
+openBtn.addEventListener("click",()=>{
+  navBars.forEach((nav)=>{
+    nav.classList.add("visible")
+  })
+})
+
+closeBtn.addEventListener("click",()=>{
+  navBars.forEach((nav)=>{
+    nav.classList.remove("visible")
+  })
+})
